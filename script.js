080c8f69e3da4e07cc3d7d29beadea4d64fe4f47
Dilhan Chathuranga Thilakarathna <dctseeker@gmail.com>
2023-07-09 18:54:45 +0530

    test

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("sumForm");
+const resultElement = document.getElementById("result");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  // Get the input values
+  const number1 = parseInt(document.getElementById("number1").value);
+  const number2 = parseInt(document.getElementById("number2").value);
+
+  // Add the two numbers
+  const sum = number1 + number2;
+
+  // Display the result on the webpage
+  resultElement.textContent = `The sum of ${number1} and ${number2} is: ${sum}`;
+});
